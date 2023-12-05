1ff3eecfb96af5b6d0aad0045221514319817594
maksym <maksimvoevodskij81@gmail.com>
2023-12-05 20:06:22 +0100

    change db

--- db-app/Migrations/20231205190514_vendoreName.Designer.cs
+++ db-app/Migrations/20231205190514_vendoreName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using db_app.Data;
 
@@ -11,9 +12,11 @@ using db_app.Data;
 namespace db_app.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    partial class ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20231205190514_vendoreName")]
+    partial class vendoreName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ db-app/Migrations/20231205190514_vendoreName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace db_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class vendoreName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VendorName",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VendorName",
+                table: "Expenses");
+        }
+    }
+}
